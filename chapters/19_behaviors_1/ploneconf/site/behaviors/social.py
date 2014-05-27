70cabbdc05696225b70db8ab741cb849930a9a44
Matt Russell <mgrbyte@users.noreply.github.com>
2014-05-27 16:51:58 +0200

    Update social.py
    Remove duplicate import

--- chapters/19_behaviors_1/ploneconf/site/behaviors/social.py
+++ chapters/19_behaviors_1/ploneconf/site/behaviors/social.py
@@ -2,7 +2,6 @@ from plone.supermodel import model, directives
 from plone.autoform.interfaces import IFormFieldProvider
 from zope import schema
 from zope.interface import alsoProvides
-from plone.autoform.interfaces import IFormFieldProvider
 
 
 class ISocial(model.Schema):

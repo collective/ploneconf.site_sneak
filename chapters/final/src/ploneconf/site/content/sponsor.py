515b9e2d2cb6b7d847fdded84b78c08e11e4b85b
Philip Bauer <bauer@starzel.de>
2015-02-27 14:53:34 +0100

    add the last batch

--- chapters/final/src/ploneconf/site/content/sponsor.py
+++ chapters/final/src/ploneconf/site/content/sponsor.py
@@ -2,14 +2,13 @@
 from plone.app.textfield import RichText
 from plone.autoform import directives
 from plone.namedfile import field as namedfile
-from plone.supermodel.directives import fieldset
 from plone.supermodel import model
+from plone.supermodel.directives import fieldset
+from ploneconf.site import _
 from z3c.form.browser.radio import RadioFieldWidget
 from zope import schema
-from zope.schema.vocabulary import SimpleVocabulary
 from zope.schema.vocabulary import SimpleTerm
-
-from ploneconf.site import _
+from zope.schema.vocabulary import SimpleVocabulary
 
 
 LevelVocabulary = SimpleVocabulary(

0b6961de84eb6d7c201644d959095a7a56784690
Patrick Gerken <do3cc@patrick-gerken.de>
2015-02-27 17:55:23 +0100

    Adding relation from speaker to talk.
    Adding code to show how back references work

--- chapters/final/src/ploneconf/site/browser/speaker.py
+++ chapters/final/src/ploneconf/site/browser/speaker.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
-from plone.dexterity.browser.view import DefaultView
 from plone import api
-
+from plone.dexterity.browser.view import DefaultView
+from z3c.relationfield.index import dump
+from zc.relation.interfaces import ICatalog
+from zope.component import getUtility
 import logging
+
 logger = logging.getLogger(__name__)
 
 
@@ -12,4 +15,18 @@ class SpeakerView(DefaultView):
 
     def talks(self):
         """Get talks from backrelations"""
-        return []
+        retval = []
+        cat = getUtility(ICatalog)
+        int_id = dump(self.context, cat, {})
+        if not int_id:
+            return []
+        relations = cat.findRelations(dict(to_id=int_id))
+        for relation in relations:
+            if relation.isBroken():
+                retval.append(dict(href='',
+                                   title='broken reference'))
+            else:
+                obj = relation.from_object
+                retval.append(dict(href=obj.absolute_url(),
+                                   title=obj.title))
+        return retval

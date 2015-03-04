a94d5f8f9aff245f7cadbf4d7f5830b95dacaa5f
Philip Bauer <bauer@starzel.de>
2015-03-04 08:23:45 +0100

    start dexterity_3

--- chapters/08_viewlets_1_p5/src/ploneconf/site/behaviors/social.py
+++ chapters/08_viewlets_1_p5/src/ploneconf/site/behaviors/social.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from plone.autoform.interfaces import IFormFieldProvider
-from plone.supermodel import model, directives
+from plone.supermodel import directives
+from plone.supermodel import model
 from zope import schema
 from zope.interface import alsoProvides
 

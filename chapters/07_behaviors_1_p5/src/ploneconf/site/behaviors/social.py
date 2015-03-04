1e02dd27bf6ada369c77fb6be97c84763fe2f475
Philip Bauer <bauer@starzel.de>
2015-03-04 08:09:19 +0100

    add chapters 7 & 8

---
+++ chapters/07_behaviors_1_p5/src/ploneconf/site/behaviors/social.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.supermodel import model, directives
+from zope import schema
+from zope.interface import alsoProvides
+
+
+class ISocial(model.Schema):
+
+    directives.fieldset(
+        'social',
+        label=u'Social',
+        fields=('lanyrd',),
+    )
+
+    lanyrd = schema.URI(
+        title=u"Lanyrd-link",
+        description=u"Add URL",
+        required=False,
+    )
+
+alsoProvides(ISocial, IFormFieldProvider)
---
+++ chapters/08_viewlets_1_p5/src/ploneconf/site/browser/views.py
@@ -0,0 +1,60 @@
+# -*- coding: UTF-8 -*-
+from Products.Five.browser import BrowserView
+from plone.dexterity.browser.view import DefaultView
+from plone import api
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+class DemoView(BrowserView):
+    """This is a sample browser view with one method."""
+
+    def get_types(self):
+        """Returns a dict with type names and the amount of items
+        for this type in the site.
+        """
+        portal_catalog = api.portal.get_tool('portal_catalog')
+        portal_types = api.portal.get_tool('portal_types')
+        content_types = portal_types.listContentTypes()
+        results = []
+        for ct in content_types:
+            brains = portal_catalog(portal_type=ct)
+            if brains:
+                results.append({
+                    'type': ct,
+                    'qtt': len(brains),
+                })
+            else:
+                logger.info("No elements of type {0}".format(ct))
+
+        return results
+
+
+class TalkView(DefaultView):
+    """The default view for talks"""
+
+
+class TalkListView(BrowserView):
+    """ A list of talks
+    """
+
+    def talks(self):
+        results = []
+        portal_catalog = api.portal.get_tool('portal_catalog')
+        current_path = "/".join(self.context.getPhysicalPath())
+
+        brains = portal_catalog(portal_type="talk",
+                                path=current_path)
+        for brain in brains:
+            talk = brain.getObject()
+            results.append({
+                'title': brain.Title,
+                'description': brain.Description,
+                'url': brain.getURL(),
+                'audience': ', '.join(talk.audience),
+                'type_of_talk': talk.type_of_talk,
+                'speaker': talk.speaker,
+                'uuid': brain.UID,
+                })
+        return results

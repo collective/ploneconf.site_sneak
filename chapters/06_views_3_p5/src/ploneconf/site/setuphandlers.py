d0f49f0e793048eef9757686a233ec6551e2e36e
Fulvio Casali <fulviocasali@gmail.com>
2015-10-08 15:04:50 +0000

    Update views_3.

--- chapters/06_views_3_p5/src/ploneconf/site/setuphandlers.py
+++ chapters/06_views_3_p5/src/ploneconf/site/setuphandlers.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
+from Products.CMFPlone.interfaces import INonInstallable
+from zope.interface import implementer
 
 
-def isNotCurrentProfile(context):
-    return context.readDataFile('ploneconfsite_marker.txt') is None
+@implementer(INonInstallable)
+class HiddenProfiles(object):
+
+    def getNonInstallableProfiles(self):
+        """Hide uninstall profile from site-creation and quickinstaller"""
+        return [
+            'ploneconf.site:uninstall',
+        ]
 
 
 def post_install(context):
     """Post install script"""
-    if isNotCurrentProfile(context):
+    if context.readDataFile('ploneconfsite_default.txt') is None:
         return
     # Do something during the installation of this package
+
+
+def uninstall(context):
+    """Uninstall script"""
+    if context.readDataFile('ploneconfsite_uninstall.txt') is None:
+        return
+    # Do something during the uninstallation of this package
